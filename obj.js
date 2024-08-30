ed27ad86a46d655073463b1e73cd0bb3773babac
hima-2004 <himaswetha04@gmail.com>
2024-08-30 12:58:46 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,49 @@
+const pi =3.14159;
+console.log(pi);
+let age=25;
+const name="john";
+var isStudent=true;
+let person={
+    firstName:"John",
+    lastName:"Doe",
+    age:25,
+    isStudent:true
+};
+console.log(name,age,isStudent);
+console.log(person);
+let a=30;
+let r=pi*a**2;
+console.log("area of circle=",r);
+let b=40;
+let h=2;
+let an=0.5*b*h;
+console.log("area oftriangle",a);
+let aa=18;
+let hs=aa*aa;
+console.log("area of rectangle",hs);
+let ren=3;
+let v=ren*ren;
+console.log("area of square",v);
+let ds=35;
+let p=pi*ds**2/2;
+console.log("area of semicicle",p);
+let c=3;
+let g=c*3;
+console.log("area of cube",v);
+
+let NAME="Hima";
+let sub1=80;
+let sub2=98;
+let sub3=90;
+let sub4=98;
+let sub5=85;
+let marks=sub1+sub2+sub3+sub4+sub5;
+let average=(sub1+sub2+sub3+sub4+sub5)/5;
+console.log("STUDENT NAME=",name);
+console.log("TAMIL=",sub1);
+console.log("ENGLISH=",sub2);
+console.log("MATHS=",sub3);
+console.log("SCIENCE",sub4);
+console.log("SOCIAL SCIENCE",sub5);
+console.log("TOTAL MARKS:",marks);
+console.log("AVERAGE MARKS=",average);
