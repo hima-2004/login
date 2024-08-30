46d60d0e22ea3983bf045b83b773d03beff98ff3
hima-2004 <himaswetha04@gmail.com>
2024-08-30 13:02:53 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,35 @@
+function add(){
+    let a=Number(document.getElementById("value1").value);
+    let b=Number(document.getElementById("value2").value);
+    let c=a+b;
+    console.log("Result:",c);
+    document.getElementById("Result"),innerHTML="Sum of ""+a" and "+b" is "+c";
+}
+function sub(){
+    let a=Number(document.getElementById("value1").value);
+    let b=Number(document.getElementById("value2").value);
+    let c=a-b;
+    console.log("Result:",c);
+    document.getElementById("Result"),innerHTML="Subtraction of ""+a" and "+b" is "+c";
+}
+function mul(){
+    let a=Number(document.getElementById("value1").value);
+    let b=Number(document.getElementById("value2").value);
+    let c=a*b;
+    console.log("Result:",c);
+    document.getElementById("Result"),innerHTML="multiplication of ""+a" and "+b" is "+c";
+}
+function div(){
+    let a=Number(document.getElementById("value1").value);
+    let b=Number(document.getElementById("value2").value);
+    let c=a/b;
+    console.log("Result:",c);
+    document.getElementById("Result"),innerHTML="division of ""+a" and "+b" is "+c";
+}
+function exp(){
+    let a=Number(document.getElementById("value1").value);
+    let b=Number(document.getElementById("value2").value);
+    let c=a+b;
+    console.log("Result:",c);
+    document.getElementById("Result"),innerHTML="exponent of ""+a" and "+b" is "+c";
+}
